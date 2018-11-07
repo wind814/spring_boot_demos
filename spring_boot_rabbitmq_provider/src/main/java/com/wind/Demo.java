fce8e8a54cfd0b1a2f9c675f794d9bfafd46a653
wind814 <wanghainanwing@126.com>
2018-11-07 15:57:28 +0800

    修改了一些项目名称，新增rabbitMq

---
+++ spring_boot_rabbitmq_provider/src/main/java/com/wind/Demo.java
@@ -0,0 +1,4 @@
+package com.wind;
+
+public class Demo {
+}
---
+++ spring_boot_rabbitmq_provider/src/main/java/com/wind/config/RabbitMqConfig.java
@@ -0,0 +1,15 @@
+package com.wind.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+
+    @Bean
+    public Queue myQueue() {
+        return new Queue("rabbit-queue");
+    }
+
+}
